816ad621d6a64bd861b607b63973cf3ad87b5ad4
unknown <>
2022-11-08 22:36:32 +0300

    first slider non-adaptive

--- assets/js/ibg.js
+++ assets/js/ibg.js
@@ -4,3 +4,18 @@ function ibg() {
     for (let i = 0; i < ibg.length; i++) { if (ibg[i].querySelector('img')) { ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')'; } }
 }
 ibg();
+
+function negativeMargin (isX=false, isY=false, className) {
+    let obj = document.querySelectorAll(`.${className}`);
+    for (let i = 0; i < obj.length; i++) {
+        if (isX) {
+            let w  = obj[i].offsetWidth / 2;
+            obj[i].style.marginLeft = `-${w}px`;
+        }
+        if (isY) {
+            let h  = obj[i].offsetHeight / 2;
+            obj[i].style.marginTop = `-${h}px`;
+        }
+    }
+}
+negativeMargin(true,true,'about__circle-text');
